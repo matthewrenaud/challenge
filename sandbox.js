0641aa4d27d80ac4a01d002eb038ddc8a7e36137
Matt Renaud <matt@renaud.us>
2013-07-01 06:49:25 -0400

    update

--- sandbox.js
+++ sandbox.js
@@ -1,17 +1,23 @@
-var Db = require('tingodb')().Db,
-  assert = require('assert');
+var cb = require('couchbase');
 
-var db = new Db('test.db', {});
-// Fetch a collection to insert document into
-var collection = db.collection("batch_document_insert_collection_safe");
-// Insert a single document
-collection.insert([{hello:'world_safe1'}
-  , {hello:'world_safe2'}], {w:1}, function(err, result) {
-  assert.equal(null, err);
+var config = {
+  hosts : [ "localhost:8091" ],
+  bucket : "default"
+};
 
-  // Fetch the document
-  collection.findOne({hello:'world_safe2'}, function(err, item) {
-    assert.equal(null, err);
-    assert.equal('world_safe2', item.hello);
-  })
+
+cb.connect(config, function(err, cb1) {
+  if (err) {
+    console.log("Failed to connect to the cluster")
+    process.exit(1)
+  } else {
+    console.log('11112')
+    cb1.set('testkey2', 1, function (err, meta) {
+      console.log('11113')
+      cb1.get('testkey2', function (err, doc, meta) {
+
+        process.exit(1)
+      })
+    });
+  }
 });
